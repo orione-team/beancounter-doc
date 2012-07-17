fa1356b1cf813c9989451c63c11769e40071e637
moretto-nik <moretto.nik@xpeppers.com>
2012-07-17 16:15:51 +0200

    site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noname::Application.config.secret_token = '4c40d4ce490fd1c47dee1f834da1eabe77dc8031ce3b1fc37bde6b15f59e4ffb96b4fcc94a750e44fa830a1759eb456cdf5bbefb733f739d1087c71b53bb113d'
